7c18615597bce48ddc2d3516a2cf642198069bd4
mohamedmoataz392-ops <mohamedmoataz392@gmail.com>
2026-05-05 21:01:35 +0300

    login.js

--- login.js
+++ login.js
@@ -1,10 +1,20 @@
-function login() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-  if (username === "admin" && password === "1234") {
-    window.location.href = "index.html";
-  } else {
-    document.getElementById("error-msg").textContent = "Wrong username or password!";
-  }
-}
+const form = document.querySelector("form");
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const username = form.querySelector("input[type='text']").value.trim();
+    const password = form.querySelector("input[type='password']").value.trim();
+    if(username === "" || password === ""){
+        alert("Please fill all fields");
+        return;
+    }
+    if(username.length < 3){
+        alert("Username must be at least 3 characters");
+        return;
+    }
+    if(password.length < 5){
+        alert("Password must be at least 5 characters");
+        return;
+    }
+    alert("Login successful");
+    form.reset();
+});
